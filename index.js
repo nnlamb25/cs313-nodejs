225ae3d2775b8929a4a6a508acd09c072d1094ae
nnlamb25 <nnlamb25@gmail.com>
2018-03-06 16:09:48 -0700

    Everything is called one after another in one statement

--- index.js
+++ index.js
@@ -2,22 +2,14 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+express()
+    .set('port', (process.env.PORT || 5000))
+    .use(express.static(__dirname + '/public'))
+    .set('views', __dirname + '/views')
+    .set('view engine', 'ejs')
+    .get('/', (request, response) => {
+        response.render('pages/index')
+        })
+    .listen(app.get('port'), function() {
+        console.log('Node app is running on port', app.get('port'));
+        });
