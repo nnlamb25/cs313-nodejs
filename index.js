2d52cdca183ba6efb31d908f70410ea828666e68
nnlamb25 <nnlamb25@gmail.com>
2018-03-06 16:16:11 -0700

    Added group activity code for testing

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
-var app = express();
+
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const url = require('url')
+
 
 express()
-    .set('port', (process.env.PORT || 5000))
-    .use(express.static(__dirname + '/public'))
-    .set('views', __dirname + '/views')
-    .set('view engine', 'ejs')
-    .get('/', (request, response) => {
-        response.render('pages/index')
-        })
-    .listen(app.get('port'), function() {
-        console.log('Node app is running on port', app.get('port'));
-        });
+.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/math', (req, res) => {
+  	var q = url.parse(req.url, true);
+  	var qdata = q.query;
+  	var op = qdata.operation;
+  	var left = Number(qdata.leftOp);
+  	var right = Number(qdata.rightOp);
+  	var result;
+
+  	if (op == "add")
+  	{
+  		result = left + right;
+  	}
+  	else if (op == "sub")
+  	{
+  		result = left - right;
+  	}
+  	else if (op == "mult")
+  	{
+  		result = left * right;
+  	}
+  	else if (op == "div")
+  	{
+  		result = left / right;
+  	}
+
+  	res.render("result", { result : result });
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
