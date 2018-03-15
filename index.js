967e486f28162deba6c1e57162e4ad9a6aacf357
nnlamb25 <nnlamb25@gmail.com>
2018-03-15 10:07:49 -0600

    Testing json request from nodejs server

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var ejs = require('ejs');
 var fs = require('fs');
+var request = require('request');
+var jsonUrl = "http://api.openweathermap.org/data/2.5/weather?q=Brea&appid=3411e21b6e00192d6705faf2bb0b65d1";
+
+request({
+    url: jsonUrl,
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        console.log(body) // Print the json response
+    }
+})
 
 const path = require('path');
 const PORT = process.env.PORT || 5000;
@@ -14,5 +26,6 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/weather', (req, res) => {
     res.render("weather")
+    
 })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
